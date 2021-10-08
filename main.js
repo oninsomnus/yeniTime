429703cae12692c32961b5517b407d415cfb6369
Juan Diego Camacho <jdcm10@gmail.com>
2021-10-07 20:03:40 -0500

    Update main.js
    cambio en llegada nextSibling

--- main.js
+++ main.js
@@ -58,7 +58,8 @@ function countdownf(x){
 
         if (distance < 0 && countDownFabi != 946702800000) {
             clearInterval(count);
-            document.getElementById(count.id).innerHTML = "🥳 " + count.id.charAt(0).toUpperCase() + count.id.slice(1) + " llegóóóóóóÓóÓooÓ!!!!🎉🎉";
+		document.querySelector(count.id).nextSibling.innerHTML = "🥳 " + count.id.charAt(0).toUpperCase() + count.id.slice(1) + " llegóóóóóóÓóÓooÓ!!!!🎉🎉";
+            //document.getElementById(count.id).innerHTML = "🥳 " + count.id.charAt(0).toUpperCase() + count.id.slice(1) + " llegóóóóóóÓóÓooÓ!!!!🎉🎉";
         } else if (countDownFabi == 946702800000) {
             document.getElementById('fabi').innerHTML = '🤷‍♂️'
         }
